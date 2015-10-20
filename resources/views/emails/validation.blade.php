085f9da4d2420d71576740413d0cdb35c7051dbe
KaroAntonio <karoantonio@gmail.com>
2015-10-20 11:34:35 -0400

    Prune Validation Email

--- resources/views/emails/validation.blade.php
+++ resources/views/emails/validation.blade.php
@@ -1,1 +1,4 @@
-click here to validate your email {{ URL::to('validate/' . Auth::user()->validation_key) }}
+<!--click here to validate your email {{ URL::to('validate/' . Auth::user()->validation_key) }}-->
+<a href="{{ URL::to('validate/' . Auth::user()->validation_key) }}">click here to validate your email</a>
+<br>
+<a href="https://www.youtube.com/watch?v=ZXsQAXx_ao0">you tell em</a>
